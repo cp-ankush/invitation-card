e2447060a794ac2e3279e4aa68d7cde95bfff05a
cp-ankush <cpankush7@gmail.com>
2022-01-23 17:11:44 +0530

    fix

--- components/textContent.js
+++ components/textContent.js
@@ -11,15 +11,18 @@ function TextContent({ ref1 }, ref) {
         <p className="text-details-location">Berhampur, Odisha</p>
         <p className="text-details-date">Friday, 18th Feb, 2022</p>
         <Timer />
-        <div className="card-action black-glass-effect black-theme" onClick={() => {
-          console.log("hello")
-          ref1?.current?.style?.visibility = 'visible';
-          ref1?.current?.style?.opacity = '1';
-          ref1?.current?.style?.zIndex = '0';
-          ref?.current?.style?.visibility = 'hidden';
-          ref?.current?.style?.opacity = '0';
-          ref?.current?.style?.zIndex = '-1';
-        }}>
+        <div
+          className="card-action black-glass-effect black-theme"
+          onClick={() => {
+            console.log("hello");
+            ref1.current.style.visibility = "visible";
+            ref1.current.style.opacity = "1";
+            ref1.current.style.zIndex = "0";
+            ref.current.style.visibility = "hidden";
+            ref.current.style.opacity = "0";
+            ref.current.style.zIndex = "-1";
+          }}
+        >
           Groom and Bride
         </div>
       </div>
